f2a5cd5eec868e15d7f3c09bb7bcd0fb0d68d805
nuster1128 <wfzhangzeyu@163.com>
2022-03-16 19:36:05 +0800

    First Commit
    This is the first commit.

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "nusterTool",
+    version = "0.0.1",
+    keywords = ("pip", "toolkit"),
+    description = "This is a toolkit for nuster.",
+    long_description = "This is a toolkit for nuster.",
+    license = "MIT Licence",
+
+    url = "https://github.com/fengmm521/pipProject",
+    author = "mage",
+    author_email = "mage@woodcol.com",
+
+    packages = find_packages(),
+    include_package_data = True,
+    platforms = "any",
+    install_requires = []
+)
